4269417473731af2c671e5421f9b2b345f59a820
JumarySanchez <sanchezjumary@gmail.com>
2020-06-23 22:35:22 -0400

    challenge 1 and 2

---
+++ main.js
@@ -0,0 +1,29 @@
+function callChallengeBanner(num){
+    num=num+1
+    console.log("######"+ " " + "challenge" + num + " " + "######")
+}
+callChallengeBanner(0)
+
+
+
+function printNumbers(){
+let num= 0
+while(num<=5){
+    num=num+1
+    console.log(num -1)
+    } 
+}
+printNumbers()
+callChallengeBanner(1)
+
+
+function SixForPram(param){
+    let num= 0
+
+    while(num<= param){
+        num= num+1
+        console.log(num -1)
+    }
+}
+SixForPram(6)
+callChallengeBanner(2)
